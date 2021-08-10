5eef41b5869d82118edd93f5b8ecb74914956e90
Apisit Maneerat <Mixko50@users.noreply.github.com>
2021-08-10 23:37:32 +0700

    Finished

--- SwiftUIAsyncImage/ContentView.swift
+++ SwiftUIAsyncImage/ContentView.swift
@@ -7,10 +7,71 @@
 
 import SwiftUI
 
+extension Image {
+    func ImageModifier() -> some View {
+        self
+            .resizable()
+            .scaledToFit()
+            .frame(maxWidth: 256)
+    }
+    
+    func iconModifier() -> some View {
+        self
+            .ImageModifier()
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+}
+
 struct ContentView: View {
+    private let imageURL: String = "https://iconape.com/wp-content/files/zy/291859/png/291859.png"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ///MARK 1. Basic
+        
+        //AsyncImage(url: URL(string: imageURL))
+        
+        ///MARK 2 - Scale AsyncImage
+        //Default is 1.0
+        //AsyncImage(url: URL(string: imageURL), scale: 3.0)
+        
+        //MARK 3 - Placeholder
+//        AsyncImage(url: URL(string: imageURL)) { image in
+//            image
+//                .ImageModifier()
+            ///if ImageURL is can't show the image. Placeholder will showed instead
+//        } placeholder: {
+//            Image(systemName: "photo.circle.fill")
+//                .iconModifier()
+//        }
+//        .padding(40)
+//        AsyncImage(url: URL(string: imageURL)) { phase in
+            ///Success: Image loaded
+            ///Failure: Image fail with error message
+            ///EMPTY: No image is loaded
+//            if let image = phase.image {
+//                image.ImageModifier()
+//            } else if phase.error != nil {
+//                Image(systemName:"ant.circle.fill").iconModifier()
+//            } else {
+//                Image(systemName: "photo.circle.fill").iconModifier()
+//            }
+//        }.padding(40)
+        AsyncImage(url: URL(string: imageURL), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) { phase in
+            switch phase {
+            case .success(let image):
+                image.ImageModifier()
+//                    .transition(.move(edge: .top))
+//                    .transition(.slide)
+                    .transition(.scale)
+            case .failure(_):
+                Image(systemName: "ant.circle.fill").iconModifier()
+            case .empty:
+                Image(systemName: "photo.circle.fill").iconModifier()
+            @unknown default:
+                ProgressView()
+            }
+        }.padding(40)
     }
 }
 
